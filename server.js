823fd356162cb6f3bfe00c04693c4860cf893bb2
jamesj995 <mrjam7@gmail.com>
2021-07-23 01:39:16 -0500

    fixed pathing bug, code rework in progress

--- server.js
+++ server.js
@@ -2,47 +2,36 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 // Sets up the Express App
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.static(__dirname + '/public'));
 
 // Sets up middleware to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
 
-//GET API - read db.json file and return all saved notes as JSON
-app.get("/api/notes", (req, res) =>
-  fs.readFile("./Develop/db/db.json", function (err, data) {
-    if (err) throw err;
-    const results = JSON.parse(data);
-    res.json(results);
-  })
+//HTML Routes
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
 //POST API - adds requested note to the notes database
-app.post("/api/notes", (req, res) => {
-  let reqNote = req.body;
-  reqNote.id = uuidv4();
-  let currentNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"));
-  currentNotes.push(reqNote);
-  fs.writeFileSync("./Develop/db/db.json", JSON.stringify(currentNotes));
-  return console.log("Added new note!");
-});
+app.get("/api/notes", (req, res) => {
+  fs.readFile("public/db/db.json", function (err, data) {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    res.json(notes);
+  })
 
-//HTML Routes
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, "./Develop/notes.html"))
-);
-app.get('/*', (req, res) =>
-  res.sendFile(path.join(__dirname, "./Develop/index.html"))
-);
+})
 
-app.use((req, res) => {
-  res.status(404).end();
-});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
