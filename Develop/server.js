791fc84909eaeb32d06a6c1350a706af8c8dee3c
jamesj995 <mrjam7@gmail.com>
2021-07-22 22:15:32 -0500

    notes

--- Develop/server.js
+++ Develop/server.js
@@ -15,3 +15,6 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "notes.html"))
 );
 app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+
+//GET API - read db.json file and return all saved notes as JSON
+app.get("/api/update-review", (req, res) => res.json(movies));
