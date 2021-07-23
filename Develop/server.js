ffff5bc188bbe2feb959b5bf6bf32d3209145365
jamesj995 <mrjam7@gmail.com>
2021-07-22 21:53:06 -0500

    added server.js

--- Develop/server.js
+++ Develop/server.js
@@ -15,25 +15,3 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "notes.html"))
 );
 app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
-
-//API Routes
-app.get("/api/notes", (req, res) => {
-  const chosen = req.params.character;
-
-  console.log(chosen);
-
-  for (let i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
-});
-
-function readDb() {
-  fs.writeFile("db/db.json", JSON.stringify(notes, "\t"), (err) => {
-    if (err) throw err;
-    return true;
-  });
-}
